0497509b2d590671b7f397653b5eac8dee64d1f2
Nurina Junus <nurina.junus@gmail.com>
2022-03-03 19:20:39 -0700

    commit tree 32e9f397c5e8ef4ef94ad795a1aa5584367f3c78

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+      bakeries = Bakery.all
+      bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+      bakery = Bakery.find(params[:id])
+      bakery.to_json( only: [:name], include: {
+        baked_goods: { only: [:name, :price] }
+      })
+  end
+
+  get '/baked_goods/by_price' do
+      baked_goods = BakedGood.all.order(price: :desc)
+      baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+      baked_good = BakedGood.all.order(price: :desc).first
+      baked_good.to_json( only: [:name, :price] )
+  end
 
 end
